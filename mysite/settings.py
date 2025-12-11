89a8c77589605dfda0dce2b84fdd5bc92a401ccc
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-12-11 22:39:14 +0100

    debug configurado

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_seed',
     'django_bootstrap5',
     'django_bootstrap_icons',
+    'debug_toolbar',
 ]
 
 AUTH_USER_MODEL = 'arodj.User' # Sin esto no podemos migrar nuestros usuarios 
@@ -53,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = ["127.0.0.1"]
 
 ROOT_URLCONF = 'mysite.urls'
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('', include('arodj.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
